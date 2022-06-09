87200591ef19c48bf98471652d076f013d8d77a3
tapasmaity <tapasmaity774@gmail.com>
2022-06-09 18:42:21 +0530

    charts use

--- src/screens/admin-container/Home.js
+++ src/screens/admin-container/Home.js
@@ -9,9 +9,123 @@ import Icon from 'react-native-vector-icons/Feather';
 import { CustomBtn } from '../../component/allButton/CustomBtn';
 import Badges from '../../component/badge/badges';
 import MenuCard from '../../component/comon-component/MenuCard';
-// const myIcon = <Icon name="rocket" size={30} color="#900" />;
+import { PieChart } from 'react-native-svg-charts'
+import { StackedBarChart } from 'react-native-svg-charts';
+
+
+// Menu Card Data---------------!>
+const menuCardData = [
+    {
+        menuImage: require('../../assets/images/card-img/quiz.png'),
+        title: 'Play Quiz'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/money.png'),
+        title: 'Fees Due'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/assignment.png'),
+        title: 'Assignment'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/holiday.png'),
+        title: 'School Holiday'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/calendra.png'),
+        title: 'Time Table'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/results.png'),
+        title: 'Result'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/date_sheet.png'),
+        title: 'Date Sheet'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/doubts.png'),
+        title: 'Ask Doubts'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/gallery.png'),
+        title: 'School Gallery'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/leave.png'),
+        title: 'Leave Application'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/event.png'),
+        title: 'Events'
+    },
+    {
+        menuImage: require('../../assets/images/card-img/settings.jpg'),
+        title: 'Settings'
+    }
+]
 export default function Home({ navigation }) {
-    const [showPsw, setShowPsw] = useState(true)
+    const [showPsw, setShowPsw] = useState(true);
+
+    /*
+    * Bar
+    * Chart
+    */
+    const barData = [
+        {
+            month: new Date(2015, 0, 1),
+            apples: 3840,
+            bananas: 1920,
+            cherries: 960,
+            dates: 400,
+            oranges: 400,
+        },
+        {
+            month: new Date(2015, 1, 1),
+            apples: 1600,
+            bananas: 1440,
+            cherries: 960,
+            dates: 400,
+        },
+        {
+            month: new Date(2015, 2, 1),
+            apples: 640,
+            bananas: 960,
+            cherries: 3640,
+            dates: 400,
+        },
+        {
+            month: new Date(2015, 3, 1),
+            apples: 3320,
+            bananas: 480,
+            cherries: 640,
+            dates: 400,
+        },
+    ]
+
+    const colors = ['#7b4173', '#a55194', '#ce6dbd', '#de9ed6']
+    const keys = ['apples', 'bananas', 'cherries', 'dates']
+
+
+    /*
+    * Pie
+    * Chart
+    */
+    const data = [50, 40, 10]
+
+    const randomColor = () => ('#' + ((Math.random() * 0xffffff) << 0).toString(16) + '000000').slice(0, 7)
+
+    const pieData = data
+        .filter((value) => value > 0)
+        .map((value, index) => ({
+            value,
+            svg: {
+                fill: randomColor(),
+                onPress: () => console.log('press', index),
+            },
+            key: `pie-${index}`,
+        }))
+
 
     return (
         <ScrollView style={[cs.f1]}>
@@ -31,7 +145,7 @@ export default function Home({ navigation }) {
                 <View style={[cs.displayBetween]}>
                     <View>
                         <Text style={[cs.font22, cs.colorWhite, cs.textBold]}>Hi Akshey</Text>
-                        <Text style={[cs.colorWhite]}>Class XI-B  |  Roll no: 04</Text>
+                        <Text style={[cs.colorWhite]}>Class XI-B  |  Admisson no: 04242</Text>
                         <View style={[cs.mt1]}>
                             <Badges
                                 title='2020-2021'
@@ -47,21 +161,33 @@ export default function Home({ navigation }) {
             <View style={[cs.boxRoundTop, { marginTop: -30 }]}>
                 <View style={styles.cartTopContainer}>
                     <TouchableOpacity onPress={() => navigation.navigate('Attendance')} style={[styles.topCard, cs.boxShadow1]} >
-                        <Image source={attenDanceImg} alt="logo" />
+                        {/* <Image source={attenDanceImg} alt="logo" />
+                        <Text style={[cs.font30, cs.textBold, cs.mt1]}>80.39%</Text>
+                        <Text>Attendance</Text> */}
+                        <PieChart style={{ height: 100 }} data={pieData} />
+
                         <Text style={[cs.font30, cs.textBold, cs.mt1]}>80.39%</Text>
                         <Text>Attendance</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => navigation.navigate('Fees')} style={[styles.topCard, cs.boxShadow1]}>
-                        <Image source={feesDueImg} alt="logo" />
-                        <Text style={[cs.font30, cs.textBold, cs.mt1]}>&#8377; 6500</Text>
-                        <Text>Fees Due</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('ErrorPage')} style={[styles.topCard, cs.boxShadow1]}>
+                        {/* <Image source={feesDueImg} alt="logo" /> */}
+                        <StackedBarChart
+                            style={{ height: 100 }}
+                            keys={keys}
+                            colors={colors}
+                            data={barData}
+                            showGrid={false}
+                            // contentInset={{ top: 30, bottom: 30 }}
+                        />
+                        <Text style={[cs.font30, cs.textBold, cs.mt1]}>Last 4</Text>
+                        <Text>Exam Reacord</Text>
                     </TouchableOpacity>
                 </View>
                 <ScrollView
                     nestedScrollEnabled={true}
                     showsVerticalScrollIndicator={false}
                     style={{ height: HEIGHT / 2 }}>
-                    <MenuCard />
+                    <MenuCard menuCardData={menuCardData}/>
                 </ScrollView>
             </View>
         </ScrollView>
@@ -93,8 +219,7 @@ const styles = StyleSheet.create({
         // borderColor: styleData.colorPrimary300,
         backgroundColor: 'white',
         width: WIDTH / 2.4,
-        paddingVertical: 15,
-        paddingLeft: 15,
+        padding: 15,
         borderRadius: 15,
         margin: 5
     },
